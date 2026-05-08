3e93f6ba22f49c8cf7ced3ca249265acb058f1d4
Minkyu Lee <minkyu-lee@hotmail.com>
2026-05-08 18:33:37 -0400

    Initial Commit

---
+++ focus-timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  focus-timer
+//
+//  Created by minkyu on 5/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ focus-timer/focus_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  focus_timerApp.swift
+//  focus-timer
+//
+//  Created by minkyu on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct focus_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
